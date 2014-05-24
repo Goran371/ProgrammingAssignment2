a7fc936582b0789a02a117dc436ea15f0a6c814a
Goran Strangmark <goran_strangmark@hotmail.com>
2014-05-24 11:34:00 -0400

    better comments and variable matrix sizes

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## makeCacheMatrix creates three function createMatrix, storeMatrix and retrieveMatrix
-## createMatrix generates a matrix and fills it with random numbers and returns it
-## storeMatrix stores a matrix in cache
-## retrieveMatrix returns the matrix that is currently stored in cache
+## makeCacheMatrix creates three functions createMatrix, storeMatrix and retrieveMatrix
+## * createMatrix can be used to create a matrix (casheSolve will use it)
+## * storeMatrix stores a matrix passed to it in cache
+## * retrieveMatrix returns the matrix that is currently stored in cache
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## mA is the cashe storage
         mA <- NULL
-        createMatrix <-function(){
-                mA <<- matrix(rnorm(25),5,5)
+        ## * createMatrix generates a jxj matrix and fills it with random numbers and returns it
+        createMatrix <-function(j){
+                mA <<- matrix(rnorm(j^2),j,j)
+                ## mA will hold the original not inverted matrix in the makeCasheMatrix environment
         }
+        ## * storeMatrix stores a matrix passed to it in cache (makeCasheMatrix will pass the 
+        ## inverted matrix of the matrix created in createMatrix)
         storeMatrix <-function(y){
                 mA <<- y
+                ## mA will hold the inverted matrix in the makeCasheMatrix environment
         }
+        ## * retrieveMatrix returns the matrix that is currently stored in cache (the inverted one put there 
+        ## storeMatrixby)        
         retrieveMatrix <-function(){
                 mA
+                ## "picking up" the inverted matrix from cache
         }
+        ## makeCasheMatrix returns a list of functions callable by $name
         list (createMatrix=createMatrix,
               storeMatrix = storeMatrix,
               retrieveMatrix=retrieveMatrix)
 }
 
 
-## for every fith turn in the main loop in cacheSolve uses createMatrix from makeCacheMatrix to
-## obtain a new matrix, inverses ## it and stored the inversed matrix in cache using storeMatrix
-## for the 4 subsequent turns cacheSolve used retrieveMatrix to obtain the already inversed matrix from cache
+## for every fith turn in the main loop in cacheSolve, it calls createMatrix from makeCacheMatrix
+## to obtain a new matrix which it then inverses using the solve function 
+## and then stores it in cache using storeMatrix
+## for the 4 subsequent turns cacheSolve used retrieveMatrix to obtain the already inversed 
+## matrix from cache
+## this is done to demonstrate how cache can be used to avoid unnecessary repetion of computation
 ## after 25 turns in the loop it stops
 
 cacheSolve <- function(x, ...) {
         i <- 25
+        ## k will be used to control matrix size 2by; 3by3; 4by4; 5by5; 6by6
+        k <- 1
         while (i>0) {
+                ## A will be our original matrix and Ainv its inverse
                 A<-NULL
                 Ainv<-NULL
                 print("new turn in the loop, matrices set to NULL")
-                print(A)
-                print(Ainv)
+                
+                ## (i/5)%%1 returns the fraction of i/5; e.g if i is 22 it returns 0.4
                 if((i/5)%%1==0){
-                        A <- x$createMatrix() 
+                        ## this will be true for i equal to 25, 20, 15...
+                        k = k+1
+                        A <- x$createMatrix(k) 
                         print("new matrix retrieved from cache")
                         print(A)
                         Ainv<-solve(A)
@@ -49,6 +65,10 @@ cacheSolve <- function(x, ...) {
                         print("inversed matrix retrieved from cache")
                         print(Ainv)
                 }
-                i =i-1
+                i = i-1
         }
 }
+
+## use this to test
+## z<-makeCacheMatrix()
+## cacheSolve(z)
